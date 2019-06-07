fe59efc6a5cf8d14f29f9e300d3c94f1739c555f
Simon <semen.metelev@gmail.com>
2019-06-07 11:01:55 -0400

    Add policy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {FaqComponent} from './faq/faq.component';
 import {DeliveryComponent} from './delivery/delivery.component';
+import {PolicyComponent} from './policy/policy.component';
 
 const routes: Routes = [
   {path: 'about' , component: AboutComponent},
   {path: 'faq', component: FaqComponent},
-  {path: 'delivery', component: DeliveryComponent}
+  {path: 'delivery', component: DeliveryComponent},
+  {path: 'policy', component: PolicyComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FaqComponent } from './faq/faq.component';
 import { DeliveryComponent } from './delivery/delivery.component';
+import { PolicyComponent } from './policy/policy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     FaqComponent,
-    DeliveryComponent
+    DeliveryComponent,
+    PolicyComponent
   ],
   imports: [
     BrowserModule,
