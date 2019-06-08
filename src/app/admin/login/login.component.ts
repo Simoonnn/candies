6c45ac564ba5f68fe849a314ca2deca5f50f5f2d
Simon <semen.metelev@gmail.com>
2019-06-08 22:09:39 +0300

    Add initial styling and grid system

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -12,4 +12,7 @@ export class LoginComponent {
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required]
   });
+  onSubmit() {
+    console.log('Form submitted');
+  }
 }
