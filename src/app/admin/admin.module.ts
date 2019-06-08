2f71ea518f27cd02cc7a163a2be64fffbbd8bcf1
Simon <semen.metelev@gmail.com>
2019-06-08 15:05:17 +0300

    Add back-end-less forms

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    ReactiveFormsModule
+]
 })
 export class AdminModule { }
---
+++ src/app/admin/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  constructor(private fb: FormBuilder) { }
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+}
