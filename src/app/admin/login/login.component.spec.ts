2488bb8f8b70c199dbd1a0561d39c6a898bd8f32
Simon <semen.metelev@gmail.com>
2019-06-09 09:18:00 -0400

    Add testing

--- src/app/admin/login/login.component.spec.ts
+++ src/app/admin/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ ReactiveFormsModule, HttpClientModule ],
     })
     .compileComponents();
   }));
@@ -18,8 +21,22 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  /*
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+   */
+  it('form control will be invalid and the test will fail', () => {
+    const form = component.form;
+    const email = form.get('email');
+    email.setValue('Invalid');
+    expect(email.valid).toBeFalsy();
+  });
+  it('should fail', () => {
+    const form = component.form;
+    const password = form.get('password');
+    password.setValue('');
+    expect(password.valid).toBeFalsy();
+  });
+
 });
