22992170a876cd3ac2e3f753f0efe7f229efb574
Simon <semen.metelev@gmail.com>
2019-06-10 08:37:03 -0400

    Add digest fetching

--- src/app/admin/create/create.component.ts
+++ src/app/admin/create/create.component.ts
@@ -5,7 +5,10 @@ import { url } from '../../../../base-url';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create',
@@ -13,6 +16,7 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+  public invalidName = false;
 
   constructor(private fb: FormBuilder, private isLogged: IsLoggedInService,
               private router: Router, private http: HttpClient) { }
@@ -23,6 +27,31 @@ export class CreateComponent implements OnInit {
     price: ['', [Validators.required, Validators.maxLength(6)]]
   });
   ngOnInit() {
+    // Send ajax requests sensibly
+    const nameInput = document.getElementById('name');
+    const requests = fromEvent(nameInput, 'input').pipe(
+      // @ts-ignore
+      map((e) => e.target.value),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(() => ajax({url: url + 'api/items/generate_digest', method: 'post',
+        // @ts-ignore
+      body: {name: nameInput.value}}))
+    );
+    requests.subscribe((res) => {
+      const response = res.response;
+      const success = response.success;
+      if (success === 'true') {
+        // handle success
+        const el = document.getElementById('input_text');
+        // @ts-ignore
+        el.value = response.digest;
+        console.log('Done');
+      } else {
+        // handle failure
+        this.invalidName = true;
+      }
+    });
     this.isLogged.check().subscribe((res) => {
       if (!isLoggedInHelper(res)) {
         this.router.navigateByUrl('/admin');
