9dc6753caa9ea80ab0610542a675dfd9bbdd884b
Simon <semen.metelev@gmail.com>
2019-06-10 04:02:32 -0400

    Initial create page

--- src/app/admin/create/create.component.ts
+++ src/app/admin/create/create.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IsLoggedInService } from '../is-logged-in.service';
 import { isLoggedInHelper } from '../helper';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -10,8 +11,13 @@ import {Router} from '@angular/router';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private isLogged: IsLoggedInService, private router: Router) { }
-
+  constructor(private fb: FormBuilder, private isLogged: IsLoggedInService,
+              private router: Router) { }
+  form = this.fb.group({
+    name: [],
+    description: [],
+    price: []
+  });
   ngOnInit() {
     this.isLogged.check().subscribe((res) => {
       if (!isLoggedInHelper(res)) {
