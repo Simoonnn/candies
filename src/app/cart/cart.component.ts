29b73f7043a2b69c576d4610366958baf932a83f
Simon <semen.metelev@gmail.com>
2019-06-13 13:43:04 -0400

    Add stable cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CartService} from '../cart.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  public items;
+  constructor(private http: HttpClient, private router: Router, private helper: CartService) { }
 
   ngOnInit() {
+    const itemsDict = this.helper.getItemDict();
+    let idArray: Array<string>   = [];
+    for (let i in itemsDict) {
+      idArray.push(i);
+    }
+    const body = {array: idArray};
+    this.http.post('http://localhost:4200/api/items/get_specific_items', body)
+      .subscribe((res) => {
+        // @ts-ignore
+      this.items = res.items;
+    });
+  }
+  onDelete(id) {
+    this.helper.deleteItem(id);
+    window.location.reload();
   }
 
 }
