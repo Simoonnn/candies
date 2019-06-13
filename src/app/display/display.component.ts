7f6125d1bcac33816256568afa489395a83ea290
Simon <semen.metelev@gmail.com>
2019-06-13 06:25:06 -0400

    Add rxjs to handle requests properly

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import {distinctUntilChanged, switchMap, map, debounceTime, debounce} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-display',
@@ -12,30 +15,55 @@ export class DisplayComponent implements OnInit {
   public inputValue = '';
   public order = "init";
   private initOrder = this.order;
-  public perPage = "init";;
+  public perPage = "init";
   private initPerPage = this.perPage;
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.sendGetRequest();
+    const paging = document.getElementById('paging');
+    const sorting = document.getElementById('sorting');
+    const searchBox = document.getElementById('searchBox');
+    this.generateAjax(paging, 'input');
+    this.generateAjax(sorting, 'input');
+    this.generateAjax(searchBox, 'click');
+
+    this.http.get('http://localhost:4200/api/items/get',
+      { params: this.createGetRequestBody() }).subscribe((res) => {
+      // @ts-ignore
+      this.items = res.response;
+    });
+
   }
-  onSearch() {
-    this.sendGetRequest();
-  }
-  onSelect() {
-    this.sendGetRequest();
-  }
-  sendGetRequest() {
+  createGetRequestBody() {
     const body = {
       page: (this.route.snapshot.queryParams['page'] || '1'),
       limit: (this.perPage == 'init' ? '5' : this.perPage),
       phrase: this.inputValue,
       sort: (this.order == 'init' ? '' : this.order)
     };
-    this.http.get('http://localhost:4200/api/items/get', { params: body }).subscribe((res) => {
+    return body;
+  }
+  generateAjax(el, eventType) {
+    let sub;
+    if (eventType == 'click') {
+      sub = fromEvent(el, eventType).pipe(
+        map((e) => this.inputValue),
+        debounceTime(200),
+        distinctUntilChanged());
+    } else {
+      sub = fromEvent(el, eventType).pipe(
       // @ts-ignore
-      this.items = res.response;
-    })
+        map((e) => e.target.value),
+        debounceTime(300),
+        distinctUntilChanged());
+    }
+    sub.subscribe((data) => {
+      this.http.get('http://localhost:4200/api/items/get',
+        { params: this.createGetRequestBody() }).subscribe((res) => {
+        // @ts-ignore
+        this.items = res.response;
+      });
+    });
   }
 
 }
