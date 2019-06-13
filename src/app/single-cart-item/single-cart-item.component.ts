75d98f32b87d7056d69c95104811b1325b70dadb
Simon <semen.metelev@gmail.com>
2019-06-13 12:23:49 -0400

    Add single-cart-item layout and styling

--- src/app/single-cart-item/single-cart-item.component.ts
+++ src/app/single-cart-item/single-cart-item.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CartService} from '../cart.service';
 
 @Component({
-  selector: 'app-single-cart-item',
+  selector: 'single-cart-item',
   templateUrl: './single-cart-item.component.html',
   styleUrls: ['./single-cart-item.component.scss']
 })
-export class SingleCartItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SingleCartItemComponent implements OnInit{
+  @Input("item-object") object;
+  public totalPrice;
+  public quantity;
+  constructor(private helper: CartService) { }
+  ngOnInit(): void {
+    this.quantity = this.helper.findById(this.object.id);
+    this.totalPrice = this.quantity * this.object.price;
+  }
+  onQuantityChange(event) {
+    if (event.target.value > 30) {
+      event.target.value = 30;
+    }
+    this.totalPrice = event.target.value * this.object.price;
+    console.log(this.quantity);
   }
 
 }
